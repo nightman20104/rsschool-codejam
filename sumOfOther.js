7e448afa24a6939bbe0889c4b9c9e02cdad59471
nightman20104 <43150077+nightman20104@users.noreply.github.com>
2018-10-28 13:44:20 +0200

    Delete sumOfOther.js

--- sumOfOther.js
+++
@@ -1,9 +0,0 @@
-function sumOfOther (array) {
-	let newArray = [];
-	for(let i=0;i<array.length;i++) {
-	let sum = array.reduce(function(x,y) {return x+y;});
-	let result = sum - array[i];
-	newArray.push(result);
-	}
-	return newArray;
-}

655cc590024929ef06ae23c73404099c4ed58ea9
nightman20104 <43150077+nightman20104@users.noreply.github.com>
2018-10-28 14:09:46 +0200

    style: white-space
    style: white-space

--- sumOfOthers.js
+++ sumOfOthers.js
@@ -1,9 +1,9 @@
 function sumOfOther (array) {
 	let newArray = [];
 	for(let i=0;i<array.length;i++) {
-	let sum = array.reduce(function(x,y) {return x+y;});
-	let result = sum - array[i];
-	newArray.push(result);
+	  let sum = array.reduce(function(x,y) {return x+y;});
+	  let result = sum - array[i];
+	  newArray.push(result);
 	}
 	return newArray;
 }
